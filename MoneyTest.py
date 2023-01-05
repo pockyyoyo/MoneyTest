d88df4e961e2d74c6009f55445d5d33d18b0708f
Oliver <oliver199799@gmail.com>
2023-01-05 14:48:57 +0800

    feat:addition feature for money in the same currency done

--- MoneyTest.py
+++ MoneyTest.py
@@ -1,4 +1,6 @@
 import unittest
+import functools
+import operator
 
 
 class Money:
@@ -16,21 +18,45 @@ class Money:
         return self.amount == other.amount and self.currency == other.currency
 
 
+class Portfolio:
+    def add(self, *moneys):
+        self.moneys.extend(moneys)
+
+    def evaluate(self, currency):
+        total = functools.reduce(operator.add, map(
+            lambda m: m.amount, self.moneys), 0)
+        return Money(total, currency)
+
+    def __init__(self):
+        self.moneys = []
+
+
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollars(self):
+        # 5 USD * 2 = 10 USD
         fiveDollars = Money(5, "USD")
         tenDollars = Money(10, "USD")
         self.assertEqual(tenDollars, fiveDollars.times(2))
 
     def testMultiplicationInEuros(self):
+        # 10 EUR * 2 = 20 EUR
         tenEuros = Money(10, "EUR")
         twentyEuros = Money(20, "EUR")
         self.assertEqual(twentyEuros, tenEuros.times(2))
 
     def testDivision(self):
+        # 4002 KRW / 4 = 1000.5 KRW
         originalMoney = Money(4002, "KRW")
         expectedMoneyAfterDivision = Money(1000.5, "KRW")
         self.assertEqual(expectedMoneyAfterDivision, originalMoney.divide(4))
+
+    def testAddition(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        fifteenDollars = Money(15, "USD")
+        portfolio = Portfolio()
+        portfolio.add(fiveDollars, tenDollars)
+        self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
 
 
 if __name__ == '__main__':
