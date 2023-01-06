58f2ebf4bb4361306a4d30192ae83fd3c8467915
pockyyoyo <50865201+pockyyoyo@users.noreply.github.com>
2023-01-06 14:38:35 +0800

    Delete 判斷基偶數(if).py

--- 判斷基偶數(if).py
+++
@@ -1,9 +0,0 @@
-x = int(input("Please enter an integer: "))
-if x < 0:
-    print('Negative Input')
-elif x % 2 == 0:
-    print('even')
-elif x % 2 == 1:
-    print('odd')
-else:
-    print('error')
